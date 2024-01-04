68fb949d18de7bc75ed948b64a911d609e8945c3
İmranov <semtexpert@gmail.com>
2024-01-04 14:45:45 +0300

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Fighter f1 = new Fighter("Emin", 15, 100, 90,30 );
+        Fighter f2 = new Fighter("Khabib", 10, 95, 100,40);
+
+        Match match = new Match(f1, f2, 70, 100);
+        match.baslayanOyuncuyuBulma();
+        match.run();
+
     }
 }
--- src/Match.java
+++ src/Match.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class Match {
+public class Match {
+    Fighter f1;
+    Fighter f2;
+    Fighter baslayanOyuncu;
+    Fighter digerOyuncu;
+
+
+    int minWeight;
+    int maxWeight;
+
+
+    Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.maxWeight = maxWeight;
+        this.minWeight = minWeight;
+        this.baslayanOyuncu = null;
+        this.digerOyuncu = null;
+    }
+
+    public void baslayanOyuncuyuBulma() {
+        double rastgele = Math.random() * 100;
+        if (rastgele < 50) {
+            this.baslayanOyuncu = f1;
+            this.digerOyuncu = f2;
+        } else {
+            this.baslayanOyuncu = f2;
+            this.digerOyuncu = f1;
+        }
+        System.out.println("Dövüşe Başlayan Oyuncumuz: " + baslayanOyuncu.name);
+    }
+
+    public void run() {
+        if (isCheck()) {
+
+            while (this.f1.health > 0 && this.f2.health > 0) {
+                System.out.println("=====YENİ ROUND ======");
+                this.f2.health = this.f1.hit(this.f2);
+
+                if (isWin()) {
+                    break;
+                }
+
+
+                if (this.f2.health <= 0) {
+                    System.out.println(this.f2.name + " NAKAVT!");
+                    break;
+                }
+                this.f1.health = this.f2.hit(this.f1);
+                if (isWin()) {
+                    break;
+                }
+                System.out.println(this.f1.name + " Sağlık " + this.f1.health);
+                System.out.println(this.f2.name + " Sağlık " + this.f2.health);
+
+
+            }
+
+        } else {
+            System.out.println("Sporcuların Sikletleri Uymuyor. ");
+
+        }
+    }
+
+    boolean isCheck() {
+        return (this.f1.weight >= minWeight && this.f1.weight <= maxWeight) && (this.f2.weight >= minWeight && this.f2.weight <= maxWeight);
+    }
+
+    boolean isWin() {
+        if (this.f1.health == 0) {
+            System.out.println(f2.name + " kazandı !");
+            return true;
+        }
+        if (this.f2.health == 0) {
+            System.out.println(this.f1.name + " kazandı !");
+            return true;
+        }
+        return false;
+    }
+
 }
